c7859d9775d1ea5d4928b84ab0d549670238cc14
Oleg Makarov <gstancox@gmail.com>
2012-07-03 17:24:44 +0300

    add post and blog class

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = '1800d00a1b3c4744d19caac6945856879c32ea7ebadacc5de8a47b4cbaa6396c7bcf42ab620ab07961449059bc84df43df9cd4c0ab03f4dec91926fe8e01a71f'
